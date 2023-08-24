64bf1cfade1e24ab894430f937ceff3fc312e032
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-08-24 14:09:29 +0200

    post views function

--- templates-parts/single-post.php
+++ templates-parts/single-post.php
@@ -1,4 +1,15 @@
 <div class="text-center">
+  
+  <?php  
+      // getting the id
+		$post_id = get_the_id();
+		// getting the views
+		$views = (int)get_post_meta($post_id, 'views', true);
+		update_post_meta($post_id, 'views', $views + 1);
+        $views = get_post_meta($post_id, 'views', true);
+		echo 'Post Views: <i class="fa fa-eye"></i>' . $views;
+  ?>
+
   <div class="m-0 "><?php the_post_thumbnail('post-thumbnail') ?></div>
   <div class="card-body">
   <h5 class="card-title"><?php the_title() ?></h5>
@@ -7,7 +18,7 @@
         $cat = get_the_category();
         if(is_array($cat)){
           foreach($cat as $cat_obj) {
-            echo '<i class="fa fa-layer-group"></i><a href="'.$cat_obj->taxonomy.'/'.$cat_obj->slug.'">'.$cat_obj->name.'</a>';
+            echo '<i class="fa-solid fa-eye"></i><a href="'.$cat_obj->taxonomy.'/'.$cat_obj->slug.'">'.$cat_obj->name.'</a>';
           }
        }
     ?>
--- templates-parts/widgets copy.php
+++ templates-parts/widgets copy.php
@@ -18,8 +18,17 @@ class My_Widget_most_posts extends WP_Widget {
 	);
 
 	public function widget( $args, $instance ) {
-		
-		
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+		echo '<div class="textwidget">';
+		echo esc_html__( $instance['text'], 'text_domain' );
+		echo '</div>';
+		echo '<div class="commentwidget">';
+		echo esc_html__( $instance['comment'], 'text_domain' );
+		echo '</div>';
+		echo $args['after_widget'];
 	}
 
 	public function form( $instance ) {
